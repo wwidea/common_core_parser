8ceb03dd873e798d70976636c78b05d08a9920e1
Aaron Baldwin <baldwina@wwidea.org>
2011-04-05 16:53:27 -0600

    working on tests

--- test/standard_test.rb
+++ test/standard_test.rb
@@ -5,4 +5,10 @@ class StandardTest < Test::Unit::TestCase
   test "gotta start somewhere" do
     assert true
   end
+  
+private
+  
+  def math_standard
+    @math_standard ||= File.expand_path()
+  end
 end
