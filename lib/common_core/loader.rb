cfb518a7c31aad9c43368537b0d0f1f79099cb20
Aaron Baldwin <baldwina@wwidea.org>
2011-04-12 13:51:31 -0600

    consolidate

--- lib/common_core/loader.rb
+++ lib/common_core/loader.rb
@@ -5,28 +5,22 @@ module CommonCore
   class Loader
     class << self
       def math_standards
-        load_standards_from_path File.expand_path('../../../data/Mathematics/StandardItems/Grade*/*.xml',  __FILE__)
+        load_elements_from_path(CommonCore::Standard, File.expand_path('../../../data/Mathematics/StandardItems/Grade*/*.xml',  __FILE__))
       end
       
       def math_domains
-        load_domains_from_path File.expand_path('../../../data/Mathematics/Standards/*.xml',  __FILE__)
+        load_elements_from_path(CommonCore::Domain, File.expand_path('../../../data/Mathematics/Standards/*.xml',  __FILE__))
       end
       
       def ela_standards
-        load_standards_from_path File.expand_path('../../../data/ELA_08302010/StandardItems/Grade*/*.xml',  __FILE__)
+        load_elements_from_path(CommonCore::Standard, File.expand_path('../../../data/ELA_08302010/StandardItems/Grade*/*.xml',  __FILE__))
       end
       
     private
       
-      def load_standards_from_path(path)
+      def load_elements_from_path(klass, path)
         (Dir.glob(path).map do |filename|
-          CommonCore::Standard.new(Nokogiri::XML(Pathname.new filename)) rescue nil
-        end).compact
-      end
-      
-      def load_domains_from_path(path)
-        (Dir.glob(path).map do |filename|
-          CommonCore::Domain.new(Nokogiri::XML(Pathname.new filename)) rescue nil
+          klass.new(Nokogiri::XML(Pathname.new filename)) rescue nil
         end).compact
       end
     end
