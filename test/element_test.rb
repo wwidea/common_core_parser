15ca85a9bac603e302320728af270b8773250ba1
Aaron Baldwin <baldwina@wwidea.org>
2011-04-13 11:12:33 -0600

    test element methods that raise RuntimeError

---
+++ test/element_test.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'test_helper'
+
+class ElementTest < Test::Unit::TestCase
+  
+  # to_s
+  test "should raise RuntimeError for to_s" do
+    assert_raise(RuntimeError) do
+      CommonCore::Element.new(Nokogiri::XML(File.read(File.expand_path('../data/math_standard.xml', __FILE__)))).to_s
+    end
+  end
+  
+  
+  # valid?
+  test "should raise RuntimeError for valid?" do
+    assert_raise(RuntimeError) do
+      CommonCore::Element.new(Nokogiri::XML(File.read(File.expand_path('../data/math_standard.xml', __FILE__)))).valid?
+    end
+  end
+end
